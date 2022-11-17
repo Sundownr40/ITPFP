d85aae09f93f814c4a76af8fb49ca3b8cd3dcbb3
CJ Blastos <115042772+Sundownr40@users.noreply.github.com>
2022-11-17 11:13:12 -0800

    Created Sources, Global Variables, and Colors

---
+++ Bouncing_Ball_FMain.py
@@ -0,0 +1,26 @@
+###############################################################################################
+# SOURCES                                                                                     #
+# (https://coderslegacy.com/python/pygame)                                                    #
+# (https://api.arcade.academy/en/latest/examples/platform_tutorial/index.html)                #
+# (https://api.arcade.academy/en/latest/examples/platform_tutorial/step_08.html)              #
+# (https://pythonprogramming.net/pygame-start-menu-tutorial/)                                 #
+#                                                                                             #
+###############################################################################################
+
+#Global Variables
+import pygame
+from pygame.locals import *
+import sys
+import random
+import time
+
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255,211,67)
+ORANGE = (255,128,0)
+PURPLE = (106, 13, 173)
+PINK = (243, 58, 106)
