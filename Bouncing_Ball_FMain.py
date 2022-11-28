6d91bb7399f7cb8bf561d464bd1d04e41cfa4b9c
CJ Blastos <115042772+Sundownr40@users.noreply.github.com>
2022-11-28 08:50:30 -0800

    Added Constants + Initialization Additions

--- Bouncing_Ball_FMain.py
+++ Bouncing_Ball_FMain.py
@@ -3,6 +3,7 @@
 # (https://coderslegacy.com/python/pygame)                                                    #
 # (https://api.arcade.academy/en/latest/examples/platform_tutorial/index.html)                #
 # (https://api.arcade.academy/en/latest/examples/platform_tutorial/step_08.html)              #
+# (https://coderslegacy.com/python/pygame-platformer-game-development/)                       #
 # (https://pythonprogramming.net/pygame-start-menu-tutorial/)                                 #
 #                                                                                             #
 ###############################################################################################
@@ -15,9 +16,17 @@ import random
 import time
 
 #Initialize Pygame
-pygame.init()
+pygame.init() 
+vec = pygame.math.Vector2 #2-Dimensional vectors (Vector2)
 
-#Colors
+#Setting up Constants
+HEIGHT = 450 #HEIGHT
+WIDTH = 400 #WIDTH
+ACC = 1 #ACCELERATION
+FRIC = -0.10 #FRICTION
+FPS = 60 #FPS
+
+#Colors. I like having a large variety 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -27,3 +36,4 @@ YELLOW = (255,211,67)
 ORANGE = (255,128,0)
 PURPLE = (106, 13, 173)
 PINK = (243, 58, 106)
+
